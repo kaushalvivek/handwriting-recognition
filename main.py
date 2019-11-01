05523a58d51f0a8ec39be8e238ad7633b61f06a2
Vivek Kaushal <vivek.kaushal@research.iiit.ac.in>
2019-11-01 15:47:44 +0530

    complete network

--- main.py
+++ main.py
@@ -10,10 +10,6 @@ class Network(object):
     self.biases = [np.random.randn(y, 1) for y in sizes[1:]]
     self.weights = [np.random.randn(y,x) for x, y in zip(sizes[:-1], sizes[1:])]
 
-  def sigmoid(a):
-    return 1/(1+np.exp(-a))
-
-
   def feedforward(self, a):
     for b,w in zip(self.biases, self.weights):
       a = sigmoid(np.dot(w, a) + b)
@@ -34,4 +30,46 @@ class Network(object):
         print("Epoch {0} complete".format(j))
 
   def update_mini_batch(self, minibatch, eta):
+    nabla_b = [np.zeros(b.shape) for b in self.biases]
+    nabla_w = [np.zeros(w.shape) for w in self.weights]
+    for x, y in mini_batch:
+      delta_nabla_b, delta_nabla_w = self.backprop(x,y)
+      nabla_b = [nb+dnb for nb, dnb in zip(nabla_b, delta_nabla_b)]
+      nabla_w = [nw+dnw for nw, dnw in zip(nabla_w, delta_nabla_w)]
+    self.weights = [w-(eta/len(mini_batch))*nw for w, nw in zip(self.weights, nabla_w)]
+    self.biases = [b-(eta/len(mini_batch))*nb for b, nb in zip(self.biases, nabla_b)]
     return
+
+  def backprop(self, x, y):
+    nabla_b = [np.zeros(b.shape) for b in self.biases]
+    nabla_w = [np.zeros(w.shape) for w in self.weights]
+    activation = x
+    activations = [x]
+    zs = []
+    for b, w in zip(self.biases, self.weights):
+      z = np.dot(w, activation)+b
+      zs.append(z)
+      activation = sigmoid(z)
+      activations.append(activation)
+    delta = self.cost_derivative(activations[-1,], y) * sigmoid_prime(zs[-1])
+    nabla_b[-1] = delta
+    nabla_w[-1] =np.dot(delta, activations[-2].transpose())
+    for l in range(2, self.num_layers):
+      z = zs[-l]
+      sp  sigmoid_prime(z)
+      delta = np.dot(self.weights[-l+1]).transpose(),delta)*sp
+      nabla_b[-l] = delta
+      nabla_w[-l] = np.dot(delta, activations[-l-1].transpose())
+
+  def evaluate(self, test_data):
+    test_results = [(np.argmax(self.feedforward(x)), y)] for (x, y) in test_data]
+    return sum(int(x==y) for (x,y) in test_results)
+
+  def cost_derivative(self, output_activations, y):
+    return (output_activations-y)
+
+def sigmoid_prime(a):
+    return sigmoid(a)* (1-sigmoid(a))
+
+def sigmoid(a):
+    return 1/(1+np.exp(-a))
